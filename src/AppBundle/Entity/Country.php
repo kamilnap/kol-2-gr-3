6cd1911a251762210b271e6dd12f2e2763381d40
Kamil Napiorkowski <kamilnapiorkowski@yahoo.com>
2015-04-10 09:43:22 +0200

    Country: klasa entity

---
+++ src/AppBundle/Entity/Country.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Country
+ */
+class Country
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
